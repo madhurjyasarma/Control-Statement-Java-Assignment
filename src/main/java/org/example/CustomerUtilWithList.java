d7d65b41d8d16db52c660b77bda9155a7486dfa6
madhurjyasarma <madhurjyasarma>
2022-12-06 10:37:30 +0530

    Git From Intelij

---
+++ src/main/java/org/example/CustomerUtilWithList.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class CustomerUtilWithList {
+    // Declare variables and Scanner
+    public static final Scanner scanner = new Scanner(System.in);
+
+
+    // Customer util code below
+
+}
---
+++ src/main/java/org/example/Singleton.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Singleton {
+//    private int idNumber;
+//
+//    public static int count = 0;
+//
+//    private Singleton() {
+//        count++;
+//        System.out.println("Object created ::: " + count);
+//    }
+
+}
